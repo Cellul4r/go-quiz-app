256b56bc1afa83a1abc0485bb00cee5e0259a5b6
cellul4r <seriousme.com@gmail.com>
2026-04-23 10:02:12 +0700

    feat: add default time for create update on profiles table

--- backend/domain/profile.go
+++ backend/domain/profile.go
@@ -12,7 +12,7 @@ type Profile struct {
 	Username  string         `json:"username" gorm:"unique;not null" validate:"required,min=1,max=20,alphanum"`
 	FullName  string         `json:"full_name" validate:"omitempty,max=100"`
 	AvatarURL string         `json:"avatar_url" validate:"omitempty,url"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime;default:now()"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"autoUpdateTime;default:now()"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
